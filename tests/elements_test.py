7f209c387f226789e8cbe981018d8a65bdb9ccce
RodeonGG <galliamovrr@gmail.com>
2022-07-09 15:01:18 +0500

    add new test, locators for checkbox

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,15 +1,16 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
-MAIN_PAGE = 'https://demoqa.com/text-box'
+TEST_BOX = 'https://demoqa.com/text-box'
+CHECK_BOX = 'https://demoqa.com/checkbox'
 
 
 class TestElements:
     class TestTextBox:
 
         def test_text_box(self, driver):
-            text_box_page = TextBoxPage(driver, MAIN_PAGE)
+            text_box_page = TextBoxPage(driver, TEST_BOX)
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_curr_addr, output_perm_addr = text_box_page.check_filled_form()
@@ -18,6 +19,15 @@ class TestElements:
             assert current_address == output_curr_addr, "the current address name does not match"
             assert permanent_address == output_perm_addr, "the permanent address does not match"
 
-
-
-
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, CHECK_BOX)
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            print(input_checkbox)
+            print(output_result)
+            assert input_checkbox == output_result
+            
